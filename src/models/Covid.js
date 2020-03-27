e9ab9b8ad30b6463e46cc9f9975583ef2a236766
thauandev <thauandepaula@icloud.com>
2020-03-27 16:12:04 -0300

    Identando o código

--- src/models/Covid.js
+++ src/models/Covid.js
@@ -1,17 +1,17 @@
-//Schema para as informações do banco
+// Schema para as informações do banco
 
 const mongoose = require('mongoose');
 
 const CovidSchema = new mongoose.Schema({
-    cases:{
-       type: String, 
-    },
-    deaths: {
-      type: String,
-    },
-    recovered: {
-      type: String,
-    },
+  cases: {
+    type: String,
+  },
+  deaths: {
+    type: String,
+  },
+  recovered: {
+    type: String,
+  },
 }, {
   timestamps: true,
 });
